3379bbca95bd5f16b87d0e78abaf55ceb1dd1a35
ernest.hope <ernest.hope@ocado.com>
2019-03-27 10:11:52 +0100

    reformatted

--- index.js
+++ index.js
@@ -1,108 +1,108 @@
 "use strict";
 
 class Vec2 {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
-  }
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
 
-  static withVec2(v) {
-    return new Vec2(v.x, v.y);
-  }
+    static withVec2(v) {
+        return new Vec2(v.x, v.y);
+    }
 
-  add(v) {
-    return new Vec2(this.x + v.x, this.y + v.y);
-  }
+    add(v) {
+        return new Vec2(this.x + v.x, this.y + v.y);
+    }
 
-  sub(v) {
-    return new Vec2(this.x - v.x, this.y - v.y);
-  }
+    sub(v) {
+        return new Vec2(this.x - v.x, this.y - v.y);
+    }
 
-  mul(s) {
-    return new Vec2(this.x * s, this.y * s);
-  }
+    mul(s) {
+        return new Vec2(this.x * s, this.y * s);
+    }
 
-  div(s) {
-    return new Vec2(this.x / s, this.y / s);
-  }
+    div(s) {
+        return new Vec2(this.x / s, this.y / s);
+    }
 
-  mod2() {
-    return this.x * this.x + this.y * this.y;
-  }
+    mod2() {
+        return this.x * this.x + this.y * this.y;
+    }
 
-  mod() {
-    return Math.sqrt(this.mod2());
-  }
+    mod() {
+        return Math.sqrt(this.mod2());
+    }
 
-  dot(v) {
-    return this.x * v.x + this.y * v.y;
-  }
+    dot(v) {
+        return this.x * v.x + this.y * v.y;
+    }
 
-  norm() {
-    return this.div(this.mod());
-  }
+    norm() {
+        return this.div(this.mod());
+    }
 }
 
 class MatA {
-  constructor(m00, m01, m02, m10, m11, m12) {
-    this.m00 = m00;
-    this.m01 = m01;
-    this.m02 = m02;
-    this.m10 = m10;
-    this.m11 = m11;
-    this.m12 = m12;
-  }
+    constructor(m00, m01, m02, m10, m11, m12) {
+        this.m00 = m00;
+        this.m01 = m01;
+        this.m02 = m02;
+        this.m10 = m10;
+        this.m11 = m11;
+        this.m12 = m12;
+    }
 
-  static identity() {
-    return new MatA(1, 0, 0, 0, 1, 0);
-  }
+    static identity() {
+        return new MatA(1, 0, 0, 0, 1, 0);
+    }
 
-  static translate(v) {
-    return new MatA(1, 0, v.x, 0, 1, v.y);
-  }
+    static translate(v) {
+        return new MatA(1, 0, v.x, 0, 1, v.y);
+    }
 
-  static scale(v) {
-    return new MatA(v.x, 0, 0, 0, v.y, 0);
-  }
+    static scale(v) {
+        return new MatA(v.x, 0, 0, 0, v.y, 0);
+    }
 
-  static rotate(t) {
-    var ct = Math.cos(t);
-    var st = Math.sin(t);
-    return new MatA(ct, st, 0, -st, ct, 0);
-  }
+    static rotate(t) {
+        var ct = Math.cos(t);
+        var st = Math.sin(t);
+        return new MatA(ct, st, 0, -st, ct, 0);
+    }
 
-  /*
-  *  [m00 m01 m02] [x]   [m00*x+m01*y+m02]
-  *  [m10 m11 m12] [y] = [m10*x+m11*y+m12]
-  *  [ 0   0   1 ] [1]   [       1       ]
-  */
-  static mMulV(m, v) {
-    return new Vec2(m.m00 * v.x + m.m01 * v.y + m.m02, m.m10 * v.x + m.m11 * v.y + m.m12);
-  }
+    /*
+    *  [m00 m01 m02] [x]   [m00*x+m01*y+m02]
+    *  [m10 m11 m12] [y] = [m10*x+m11*y+m12]
+    *  [ 0   0   1 ] [1]   [       1       ]
+    */
+    static mMulV(m, v) {
+        return new Vec2(m.m00 * v.x + m.m01 * v.y + m.m02, m.m10 * v.x + m.m11 * v.y + m.m12);
+    }
 
-  mulV(v) {
-    return MatA.mMulV(this, v);
-  }
+    mulV(v) {
+        return MatA.mMulV(this, v);
+    }
 
-  /*
-  *  [m00 m01 m02] [m00 m01 m02]   [m00*m00+m01*m10, m00*m01+m01*m11, m00*m02+m01*m12+m02]
-  *  [m10 m11 m12] [m10 m11 m12] = [m10*m00+m11*m10, m10*m01+m11*m11, m10*m02+m11*m12+m12]
-  *  [ 0   0   1 ] [ 0   0   1 ]   [       0                0                1           ]
-  */
-  static mMulM(m1, m2) {
-    return new MatA(m1.m00*m2.m00+m1.m01*m2.m10,
-      m1.m00*m2.m01+m1.m01*m2.m11,
-      m1.m00*m2.m02+m1.m01*m2.m12+m1.m02,
-      m1.m10*m2.m00+m1.m11*m2.m10,
-      m1.m10*m2.m01+m1.m11*m2.m11,
-      m1.m10*m2.m02+m1.m11*m2.m12+m1.m12,
-      0, 0, 1);
+    /*
+    *  [m00 m01 m02] [m00 m01 m02]   [m00*m00+m01*m10, m00*m01+m01*m11, m00*m02+m01*m12+m02]
+    *  [m10 m11 m12] [m10 m11 m12] = [m10*m00+m11*m10, m10*m01+m11*m11, m10*m02+m11*m12+m12]
+    *  [ 0   0   1 ] [ 0   0   1 ]   [       0                0                1           ]
+    */
+    static mMulM(m1, m2) {
+        return new MatA(m1.m00 * m2.m00 + m1.m01 * m2.m10,
+            m1.m00 * m2.m01 + m1.m01 * m2.m11,
+            m1.m00 * m2.m02 + m1.m01 * m2.m12 + m1.m02,
+            m1.m10 * m2.m00 + m1.m11 * m2.m10,
+            m1.m10 * m2.m01 + m1.m11 * m2.m11,
+            m1.m10 * m2.m02 + m1.m11 * m2.m12 + m1.m12,
+            0, 0, 1);
     }
 
     mulM(m) {
-      return MatA.mMulM(this, m);
+        return MatA.mMulM(this, m);
     }
-  }
+}
 
-  module.exports.Vec2 = Vec2;
-  module.exports.MatA = MatA;
+module.exports.Vec2 = Vec2;
+module.exports.MatA = MatA;
--- test.js
+++ test.js
@@ -3,189 +3,189 @@ var MatA = require("./index").MatA;
 var expect = require("chai").expect;
 const EPSLON = 0.00001;
 
-describe("Vec2 is a two dimensional vector", function() {
+describe("Vec2 is a two dimensional vector", function () {
 
-  describe("Vec2 can be constructed", function() {
-    it("can be constructed with no arguments to yield a zero vector",
-    function() {
-      var v = new Vec2();
-      expect(v.x).to.equal(0);
-      expect(v.y).to.equal(0);
+    describe("Vec2 can be constructed", function () {
+        it("can be constructed with no arguments to yield a zero vector",
+            function () {
+                var v = new Vec2();
+                expect(v.x).to.equal(0);
+                expect(v.y).to.equal(0);
+            });
+        it("can be constructed with two numbers", function () {
+            var v = new Vec2(1, 2);
+            expect(v.x).to.equal(1);
+            expect(v.y).to.equal(2);
+        });
+        it("can be constructed with another Vec2", function () {
+            var v = new Vec2(1, 2);
+            var n = Vec2.withVec2(v);
+            expect(n.x).to.equal(1);
+            expect(n.y).to.equal(2);
+        });
     });
-    it("can be constructed with two numbers", function() {
-      var v = new Vec2(1, 2);
-      expect(v.x).to.equal(1);
-      expect(v.y).to.equal(2);
-    });
-    it("can be constructed with another Vec2", function() {
-      var v = new Vec2(1, 2);
-      var n = Vec2.withVec2(v);
-      expect(n.x).to.equal(1);
-      expect(n.y).to.equal(2);
-    });
-  });
 
-  describe("Vec2 can add, mul, div and sub", function() {
-    it("can be added to another Vec2", function() {
-      var v1 = new Vec2(1, 2);
-      var v2 = new Vec2(3, 4);
-      var v3 = v1.add(v2);
-      expect(v3.x).to.equal(4);
-      expect(v3.y).to.equal(6);
+    describe("Vec2 can add, mul, div and sub", function () {
+        it("can be added to another Vec2", function () {
+            var v1 = new Vec2(1, 2);
+            var v2 = new Vec2(3, 4);
+            var v3 = v1.add(v2);
+            expect(v3.x).to.equal(4);
+            expect(v3.y).to.equal(6);
+        });
+        it("can be subtracted another Vec2", function () {
+            var v1 = new Vec2(1, 2);
+            var v2 = new Vec2(3, 5);
+            var v3 = v1.sub(v2);
+            expect(v3.x).to.equal(-2);
+            expect(v3.y).to.equal(-3);
+        });
+        it("can be multiplied by a scalar", function () {
+            var v1 = new Vec2(1, 2);
+            var v2 = v1.mul(5);
+            expect(v2.x).to.equal(5);
+            expect(v2.y).to.equal(10);
+        });
+        it("can be divided by a scalar", function () {
+            var v1 = new Vec2(10, 20);
+            var v2 = v1.div(5);
+            expect(v2.x).to.equal(2);
+            expect(v2.y).to.equal(4);
+        });
     });
-    it("can be subtracted another Vec2", function() {
-      var v1 = new Vec2(1, 2);
-      var v2 = new Vec2(3, 5);
-      var v3 = v1.sub(v2);
-      expect(v3.x).to.equal(-2);
-      expect(v3.y).to.equal(-3);
-    });
-    it("can be multiplied by a scalar", function() {
-      var v1 = new Vec2(1, 2);
-      var v2 = v1.mul(5);
-      expect(v2.x).to.equal(5);
-      expect(v2.y).to.equal(10);
-    });
-    it("can be divided by a scalar", function() {
-      var v1 = new Vec2(10, 20);
-      var v2 = v1.div(5);
-      expect(v2.x).to.equal(2);
-      expect(v2.y).to.equal(4);
-    });
-  });
 
-  describe("Vec2 can mod2, mod, dot and norm", function() {
-    it("can return modulus squared", function() {
-      var v1 = new Vec2(1, 2);
-      var c = v1.mod2();
-      expect(c).to.equal(5);
+    describe("Vec2 can mod2, mod, dot and norm", function () {
+        it("can return modulus squared", function () {
+            var v1 = new Vec2(1, 2);
+            var c = v1.mod2();
+            expect(c).to.equal(5);
+        });
+        it("can return modulus", function () {
+            var v1 = new Vec2(2, 3);
+            var c = v1.mod();
+            expect(c).to.be.closeTo(Math.sqrt(13), EPSLON);
+        });
+        it("can be dotted with another Vec2", function () {
+            var v1 = new Vec2(1, 2);
+            var v2 = new Vec2(3, 4);
+            var c = v1.dot(v2);
+            expect(c).to.equal(11);
+        });
+        it("can return an normalised vector of itself", function () {
+            var v1 = new Vec2(2, 3);
+            var v2 = v1.norm();
+            var ex = 2 / Math.sqrt(13);
+            var ey = 3 / Math.sqrt(13);
+            expect(v2.x).to.be.closeTo(ex, EPSLON);
+            expect(v2.y).to.be.closeTo(ey, EPSLON);
+        });
     });
-    it("can return modulus", function() {
-      var v1 = new Vec2(2, 3);
-      var c = v1.mod();
-      expect(c).to.be.closeTo(Math.sqrt(13), EPSLON);
-    });
-    it("can be dotted with another Vec2", function() {
-      var v1 = new Vec2(1, 2);
-      var v2 = new Vec2(3, 4);
-      var c = v1.dot(v2);
-      expect(c).to.equal(11);
-    });
-    it("can return an normalised vector of itself", function() {
-      var v1 = new Vec2(2, 3);
-      var v2 = v1.norm();
-      var ex = 2 / Math.sqrt(13);
-      var ey = 3 / Math.sqrt(13);
-      expect(v2.x).to.be.closeTo(ex, EPSLON);
-      expect(v2.y).to.be.closeTo(ey, EPSLON);
-    });
-  });
 
-  describe("Vec2 can be compounded", function() {
-    it("can compound functions", function() {
-        var v1 = new Vec2(2, 2);
-        var v2 = v1.add(new Vec2(3, 3)).sub(new Vec2(4, 4));
-        expect(v2.x).to.equal(1);
-        expect(v2.y).to.equal(1);
+    describe("Vec2 can be compounded", function () {
+        it("can compound functions", function () {
+            var v1 = new Vec2(2, 2);
+            var v2 = v1.add(new Vec2(3, 3)).sub(new Vec2(4, 4));
+            expect(v2.x).to.equal(1);
+            expect(v2.y).to.equal(1);
+        });
     });
-  });
 });
 
-describe("MatA is 3x3 matrix used for affine transforms", function() {
+describe("MatA is 3x3 matrix used for affine transforms", function () {
 
-  describe("MatA can be constructed", function() {
-    it("can be constructed with 6 parameters", function() {
-      var m = new MatA(1, 2, 3, 4, 5, 6);
-      expect(m.m00).equal(1);
-      expect(m.m01).equal(2);
-      expect(m.m02).equal(3);
-      expect(m.m10).equal(4);
-      expect(m.m11).equal(5);
-      expect(m.m12).equal(6);
+    describe("MatA can be constructed", function () {
+        it("can be constructed with 6 parameters", function () {
+            var m = new MatA(1, 2, 3, 4, 5, 6);
+            expect(m.m00).equal(1);
+            expect(m.m01).equal(2);
+            expect(m.m02).equal(3);
+            expect(m.m10).equal(4);
+            expect(m.m11).equal(5);
+            expect(m.m12).equal(6);
+        });
+        it("can be constructed using static identity", function () {
+            var m = MatA.identity();
+            expect(m.m00).equal(1);
+            expect(m.m01).equal(0);
+            expect(m.m02).equal(0);
+            expect(m.m10).equal(0);
+            expect(m.m11).equal(1);
+            expect(m.m12).equal(0);
+        });
+        it("can be constructed using static translate", function () {
+            var m = MatA.translate(new Vec2(10, 20));
+            expect(m.m00).equal(1);
+            expect(m.m01).equal(0);
+            expect(m.m02).equal(10);
+            expect(m.m10).equal(0);
+            expect(m.m11).equal(1);
+            expect(m.m12).equal(20);
+        });
+        it("can be constructed using static scale", function () {
+            var m = MatA.scale(new Vec2(10, 20));
+            expect(m.m00).equal(10);
+            expect(m.m01).equal(0);
+            expect(m.m02).equal(0);
+            expect(m.m10).equal(0);
+            expect(m.m11).equal(20);
+            expect(m.m12).equal(0);
+        });
+        it("can be constructed using static rotate", function () {
+            var m = MatA.rotate(Math.PI / 2);
+            var ct = Math.cos(Math.PI / 2);
+            var st = Math.sin(Math.PI / 2);
+            expect(m.m00).to.be.closeTo(ct, EPSLON);
+            expect(m.m01).to.be.closeTo(st, EPSLON);
+            expect(m.m02).equal(0)
+            expect(m.m10).to.be.closeTo(-st, EPSLON);
+            expect(m.m11).to.be.closeTo(ct, EPSLON);
+            expect(m.m12).equal(0);
+        });
     });
-    it("can be constructed using static identity", function() {
-      var m = MatA.identity();
-      expect(m.m00).equal(1);
-      expect(m.m01).equal(0);
-      expect(m.m02).equal(0);
-      expect(m.m10).equal(0);
-      expect(m.m11).equal(1);
-      expect(m.m12).equal(0);
-    });
-    it("can be constructed using static translate", function() {
-      var m = MatA.translate(new Vec2(10, 20));
-      expect(m.m00).equal(1);
-      expect(m.m01).equal(0);
-      expect(m.m02).equal(10);
-      expect(m.m10).equal(0);
-      expect(m.m11).equal(1);
-      expect(m.m12).equal(20);
-    });
-    it("can be constructed using static scale", function() {
-      var m = MatA.scale(new Vec2(10, 20));
-      expect(m.m00).equal(10);
-      expect(m.m01).equal(0);
-      expect(m.m02).equal(0);
-      expect(m.m10).equal(0);
-      expect(m.m11).equal(20);
-      expect(m.m12).equal(0);
-    });
-    it("can be constructed using static rotate", function() {
-      var m = MatA.rotate(Math.PI / 2);
-      var ct = Math.cos(Math.PI / 2);
-      var st = Math.sin(Math.PI / 2);
-      expect(m.m00).to.be.closeTo(ct, EPSLON);
-      expect(m.m01).to.be.closeTo(st, EPSLON);
-      expect(m.m02).equal(0)
-      expect(m.m10).to.be.closeTo(-st, EPSLON);
-      expect(m.m11).to.be.closeTo(ct, EPSLON);
-      expect(m.m12).equal(0);
-    });
-  });
 
-  describe("MatA can operate on vectors", function() {
-    it("can mul a vector", function() {
-      var m = MatA.rotate(Math.PI / 2);
-      var v = m.mulV(new Vec2(1, 1));
-      expect(v.x).to.be.closeTo(1, EPSLON);
-      expect(v.y).to.be.closeTo(-1, EPSLON);
+    describe("MatA can operate on vectors", function () {
+        it("can mul a vector", function () {
+            var m = MatA.rotate(Math.PI / 2);
+            var v = m.mulV(new Vec2(1, 1));
+            expect(v.x).to.be.closeTo(1, EPSLON);
+            expect(v.y).to.be.closeTo(-1, EPSLON);
+        });
+        it("can static mul a vector", function () {
+            var m = MatA.rotate(Math.PI / 2);
+            var v = new Vec2(1, 1);
+            var r = MatA.mMulV(m, v);
+            expect(r.x).to.be.closeTo(1, EPSLON);
+            expect(r.y).to.be.closeTo(-1, EPSLON);
+        });
     });
-    it("can static mul a vector", function() {
-      var m = MatA.rotate(Math.PI / 2);
-      var v = new Vec2(1, 1);
-      var r = MatA.mMulV(m, v);
-      expect(r.x).to.be.closeTo(1, EPSLON);
-      expect(r.y).to.be.closeTo(-1, EPSLON);
-    });
-  });
 
-  describe("MatA can operate on other MatA's", function() {
-    it("can mul a MatA, two identities", function() {
-      var m1 = MatA.identity();
-      var m2 = MatA.identity();
-      var mr = m1.mulM(m2);
-      expect(mr.m00).to.be.closeTo(1, EPSLON);
-      expect(mr.m01).to.be.closeTo(0, EPSLON);
-      expect(mr.m02).to.be.closeTo(0, EPSLON);
-      expect(mr.m10).to.be.closeTo(0, EPSLON);
-      expect(mr.m11).to.be.closeTo(1, EPSLON);
-      expect(mr.m12).to.be.closeTo(0, EPSLON);
+    describe("MatA can operate on other MatA's", function () {
+        it("can mul a MatA, two identities", function () {
+            var m1 = MatA.identity();
+            var m2 = MatA.identity();
+            var mr = m1.mulM(m2);
+            expect(mr.m00).to.be.closeTo(1, EPSLON);
+            expect(mr.m01).to.be.closeTo(0, EPSLON);
+            expect(mr.m02).to.be.closeTo(0, EPSLON);
+            expect(mr.m10).to.be.closeTo(0, EPSLON);
+            expect(mr.m11).to.be.closeTo(1, EPSLON);
+            expect(mr.m12).to.be.closeTo(0, EPSLON);
+        });
+        it("can mul a MatA", function () {
+            var m1 = new MatA(1, 2, 3, 4, 5, 6);
+            var m2 = new MatA(7, 8, 9, 10, 11, 12);
+            var mr = m1.mulM(m2);
+            expect(mr.m00).to.be.closeTo(27, EPSLON);
+            expect(mr.m01).to.be.closeTo(30, EPSLON);
+            expect(mr.m02).to.be.closeTo(36, EPSLON);
+            expect(mr.m10).to.be.closeTo(78, EPSLON);
+            expect(mr.m11).to.be.closeTo(87, EPSLON);
+            expect(mr.m12).to.be.closeTo(102, EPSLON);
+        });
+        it("can static mul a MatA", function () {
+            var m1 = MatA.rotate(Math.PI / 2);
+            var m2 = MatA.translate(new Vec2(10, 10));
+            var mr = MatA.mMulM(m1, m2);
+        });
     });
-    it("can mul a MatA", function() {
-      var m1 = new MatA(1, 2, 3, 4, 5, 6);
-      var m2 = new MatA(7, 8, 9, 10, 11, 12);
-      var mr = m1.mulM(m2);
-      expect(mr.m00).to.be.closeTo(27, EPSLON);
-      expect(mr.m01).to.be.closeTo(30, EPSLON);
-      expect(mr.m02).to.be.closeTo(36, EPSLON);
-      expect(mr.m10).to.be.closeTo(78, EPSLON);
-      expect(mr.m11).to.be.closeTo(87, EPSLON);
-      expect(mr.m12).to.be.closeTo(102, EPSLON);
-    });
-    it("can static mul a MatA", function() {
-      var m1 = MatA.rotate(Math.PI / 2);
-      var m2 = MatA.translate(new Vec2(10, 10));
-      var mr = MatA.mMulM(m1, m2);
-    });
-  });
 });
